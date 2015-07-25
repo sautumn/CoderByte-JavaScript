442b241dbcdbc7d0c58aa20678c2e313c0b6dae5
autumn <ansmith12@crimson.ua.edu>
2015-07-25 15:43:33 -0500

    Revert "CoderByte Exercises"
    This reverts commit 67e468b65c863a0856b5d21fab39ade717958338.

--- cb-chainevaluation.js
+++
@@ -1,8 +0,0 @@
-function evaluation(add,subtract,multiply,divide){
-	this.add = function(){;
-	this.subtract = subtract;
-	this.multiply = multiply;
-	this.divide = divide;
-}
-
-console.log((3).add(5));
